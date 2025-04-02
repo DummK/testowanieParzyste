2a3f3c8808aa7c2535b528802ecb12ff02c799b1
Filip <fpolski22@gmail.com>
2025-04-02 13:23:54 +0200

    Skończenie

--- src/main/java/testing/collection/OddNumbersExterminator.java
+++ src/main/java/testing/collection/OddNumbersExterminator.java
@@ -1,18 +1,23 @@
 package testing.collection;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class OddNumbersExterminator {
     public List<Integer> exterminate(List<Integer> numbers) {
-        if(numbers.isEmpty()) {
+        if (numbers.isEmpty()) {
             System.out.println("List is empty");
+            return numbers;
         }
         else {
-            for(int i : numbers) {
-                numbers.removeIf(n -> n % 2 != 0);
+            List<Integer> oddNumbers = new ArrayList<>();
+            for (Integer number : numbers) {
+                if (number % 2 == 0) {
+                    oddNumbers.add(number);
+                }
             }
-
+            return oddNumbers;
         }
-        return numbers;
     }
 }
--- src/test/java/testing/collection/CollectionTestSuite.java
+++ src/test/java/testing/collection/CollectionTestSuite.java
@@ -1,18 +1,50 @@
 package testing.collection;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
+import java.util.Arrays;
 import java.util.List;
 
 public class CollectionTestSuite {
+        private OddNumbersExterminator exterminator;
 
-    @Test
-    public void testOddNumbersExterminatorEmptyList() {
-        //Given
-        OddNumbersExterminator obj = new OddNumbersExterminator();
-        List<Integer> numbers = List.of();
+        @BeforeEach
+            void beforeEach() {
+                System.out.println("Test start");
+                exterminator = new OddNumbersExterminator();
+            }
 
+        @AfterEach
+            void afterEach() {
+                System.out.println("Test end");
+            }
 
+        @DisplayName("When input list is empty, then returned list should also be empty")
+            @Test
+                public void testOddNumbersExterminatorEmptyList() {
+                    //Given
+                    exterminator = new OddNumbersExterminator();
+                    List<Integer> numbers = Arrays.asList();
 
-    }
+                    //When
+                    List<Integer> result = exterminator.exterminate(numbers);
+
+                    //Then
+                    Assertions.assertTrue(result.isEmpty());
+                }
+
+        @DisplayName("When input list is not empty, then returned list should also be not empty")
+            @Test
+                public void testOddNumbersExterminatorNormalList() {
+                    //Given
+                    exterminator = new OddNumbersExterminator();
+                    List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+                    List<Integer> expectedResult = Arrays.asList(2, 4, 6, 8, 10);
+
+                    //when
+                    List<Integer> result =  exterminator.exterminate(numbers);
+
+                    //then
+                    Assertions.assertEquals(expectedResult, result);
+                }
 }
